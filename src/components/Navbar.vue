1506bd367690b367662b512b8c131208a67baf93
toshihirokato <nanamin.104@icloud.com>
2020-04-02 21:28:37 +0900

    ナビゲーションバーの作成

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text color="grey">
+      <span>Sign Out</span>
+      <v-icon right>mdi-exit-to-app</v-icon>
+    </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
