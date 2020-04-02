51777054607ae118929cd472118ea874fcc45d7c
toshihirokato <nanamin.104@icloud.com>
2020-04-02 21:24:05 +0900

    ホームページの作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+
+    <!-- <v-btn class="d-none d-md-flex d-lg-none">click me</v-btn>
+    <br>
+    <br>
+    <v-btn class="d-md-none d-lg-flex">click me</v-btn> -->
+
+    <!-- <v-btn class="d-sm-none d-md-flex">click me</v-btn> -->
+
+    <!-- アイコンとボタン -->
+    <!-- <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn text color="pink">click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span small>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+        <v-icon right>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn depressed fab small dark color="purple white--text">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn> -->
+
+    <!-- テキスト -->
+    <!-- <p class="red white--text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste illo, eaque voluptate at ab, corporis et obcaecati ducimus, sunt excepturi aperiam? Nisi animi obcaecati modi sed consectetur voluptas non nam.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, quos. Commodi incidunt, doloremque sit exercitationem nemo tempore, quidem corporis at, animi voluptatem esse minima. Eligendi dolorem blanditiis maiores incidunt aut.</p>
+    <h1 class="display-4 font-italic font-weight-bold">Massive Display</h1>
+    <h4 class="display-1">Smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-thin">This is a subheading</p>
+    <p class="caption"> This is a caption</p> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
