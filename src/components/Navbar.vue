6030651d420c4e4b81f271bfa3bbd79a149ac69b
toshihirokato <nanamin.104@icloud.com>
2020-04-04 19:10:27 +0900

    ヘッダー等のナビゲーションバーの作成

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,78 @@
 <template>
   <nav>
-    <v-toolbar flat app>
+    <v-app-bar text app>
+      <v-app-bar-nav-icon class="gray--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
       </v-toolbar-title>
     <v-spacer></v-spacer>
+
+    <!-- dropdown menu -->
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn text color="grey" v-on="on" v-bind="attrs">
+          <v-icon left>mdi-chevron-down</v-icon>
+          <span>Menu</span>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-title>{{ link.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
     <v-btn text color="grey">
       <span>Sign Out</span>
       <v-icon right>mdi-exit-to-app</v-icon>
     </v-btn>
-    </v-toolbar>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="/ninja.jpeg" alt="">
+          </v-avatar>
+          <p class="white--text subheading mt-1">The Net Ninja</p>
+        </v-flex>
+        <v-flex class="mt-4 mb-3">
+          <Popup />
+        </v-flex>
+      </v-layout>
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-title>
+            <v-icon color="white">{{ link.icon }}</v-icon>
+          </v-list-item-title>
+          <v-list-item-title class="white--text" left>
+            {{ link.text }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
+import Popup from './Popup'
 
 export default {
-
+  components: {
+    Popup
+  },
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+        { icon: 'mdi-account', text: 'Team', route: '/team' },
+      ]
+    }
+  }
 }
 </script>
 
