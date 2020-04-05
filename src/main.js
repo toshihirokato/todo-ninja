4d6c1b3bbddca52a0e0e85cac3061747dc3f83e9
toshihirokato <nanamin.104@icloud.com>
2020-04-05 13:02:55 +0900

    firebase の設定

--- src/main.js
+++ src/main.js
@@ -2,8 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAZE7EncCafg7iWxScjcOIKc09x9b8fo_Y",
+    authDomain: "todo-ninja-c9a79.firebaseapp.com",
+    databaseURL: "https://todo-ninja-c9a79.firebaseio.com",
+    projectId: "todo-ninja-c9a79",
+    storageBucket: "todo-ninja-c9a79.appspot.com",
+    messagingSenderId: "1084235949392",
+    appId: "1:1084235949392:web:88a1c8daee088948b0f855"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+
+export default db
 
 new Vue({
   router,
