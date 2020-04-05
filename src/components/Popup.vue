aa82b220af1f387b24efa0fa1bb6345947351f8e
toshihirokato <nanamin.104@icloud.com>
2020-04-05 13:06:21 +0900

    ポップアップコンポーネントの作成

---
+++ src/components/Popup.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-dialog max-width="600px">
+    <template v-slot:activator="{ on }">
+      <v-btn text class="success" v-on="on">Add new project</v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title>
+        <h2>Add a new Project</h2>
+      </v-card-title>
+
+      <v-card-text>
+        <v-form class="px-3" ref="form">
+          <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder" :rules="inputRules"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil" :rules="inputRules"></v-textarea>
+
+          <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field :value="due" label="Due Date" prepend-icon="mdi-calendar-range" v-on="on" v-bind="attrs">
+              </v-text-field>
+            </template>
+            <v-date-picker v-model="due"></v-date-picker>
+          </v-menu>
+
+          <!-- <v-dialog max-width="600px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field :value="due" label="Due Date" prepend-icon="mdi-calendar-range" v-on="on" v-bind="attrs">
+              </v-text-field>
+            </template>
+
+            <v-card>
+            <v-card-text>
+              <v-form class="px-3">
+                <v-date-picker v-model="due"></v-date-picker>
+              </v-form>
+            </v-card-text>
+
+            </v-card>
+          </v-dialog> -->
+
+          <v-btn text class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
+        </v-form>
+      </v-card-text>
+
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+// import { format } from 'date-fns/format'
+import db from '../main'
+
+export default {
+  data() {
+    return {
+      title: '',
+      content: '',
+      due: null,
+      inputRules: [
+        v => v.length >= 3 || 'Minimum length 3 characters'
+      ]
+    }
+  },
+  methods: {
+    submit() {
+      // if(this.$refs.form.valiadte()) {
+        // console.log(this.title, this.content)
+        const project = {
+          title: this.title,
+          content: this.content,
+          due: this.due,
+          person: 'The Net Ninja',
+          status: 'ongoing'
+        }
+        //! コレクションにデータを格納する
+        db.collection('projects').add(project).then((
+          console.log('added to db')
+        ))
+      // }
+    }
+  },
+  // computed: {
+  //   formattedDate() {
+  //     return this.due ? format(this.due, 'Do MMM YYYY') : ''
+  //   }
+  // }
+}
+</script>
+
+<style>
+
+</style>
